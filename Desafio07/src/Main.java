704ea52e5acfa3f2a71b8c658ec236ea4aa0f910
Nicolasod <nicolasouriquesdurante@gmail.com>
2021-08-22 21:15:20 -0300

    AttDesafio07

--- Desafio07/src/Main.java
+++ Desafio07/src/Main.java
@@ -1,2 +1,25 @@
+import model.Funcionario;
+import repository.FuncionarioRepository;
+import service.FuncionarioService;
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Funcionario funcionario = new Funcionario(2,"Jessica","90876764837",1000.0);
+
+        FuncionarioService service = new FuncionarioService();
+
+        service.save(funcionario);
+
+        funcionario.setNome(funcionario.getNome() + " - alterado");
+
+        service.update(funcionario);
+
+        FuncionarioRepository repository = new FuncionarioRepository();
+
+        repository.findAll();
+
+        repository.findById(10);
+    }
 }
