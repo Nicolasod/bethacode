5fbfad851ae1db40560fd58013365cab4c7163d2
Nicolasod <nicolasouriquesdurante@gmail.com>
2021-08-22 21:40:02 -0300

    att.Desafio07

--- Desafio07/src/Main.java
+++ Desafio07/src/Main.java
@@ -1,25 +1,42 @@
+import model.Diretor;
 import model.Funcionario;
+import repository.DiretorRepository;
 import repository.FuncionarioRepository;
+import service.DiretorService;
 import service.FuncionarioService;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Funcionario funcionario = new Funcionario(2,"Jessica","90876764837",1000.0);
+//        Funcionario funcionario = new Funcionario(12,"Raul","90876764839",1000.0);
+//
+//        FuncionarioService service = new FuncionarioService();
+//
+//        service.save(funcionario);
+//
+//        funcionario.setNome(funcionario.getNome() + " - alterado");
+//
+//        service.update(funcionario);
+//
+//        FuncionarioRepository repository = new FuncionarioRepository();
 
-        FuncionarioService service = new FuncionarioService();
 
-        service.save(funcionario);
 
-        funcionario.setNome(funcionario.getNome() + " - alterado");
+        Diretor diretor = new Diretor(1,"Gabriel","98784678376",2000.00,5);
 
-        service.update(funcionario);
+        DiretorService service = new DiretorService();
 
-        FuncionarioRepository repository = new FuncionarioRepository();
+        service.save(diretor);
+
+        diretor.setNome(diretor.getNome() + " - alterado");
+
+        service.update(diretor);
+
+        DiretorRepository repository = new DiretorRepository();
 
         repository.findAll();
 
-        repository.findById(10);
+        repository.findById(1);
     }
 }
