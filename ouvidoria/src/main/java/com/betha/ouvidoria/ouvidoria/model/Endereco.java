87b6739f93a624c4a9ec8e7b16e2efbfec1a3ae6
Nicolasod <nicolasouriquesdurante@gmail.com>
2021-08-27 21:49:14 -0300

    att2

---
+++ ouvidoria/src/main/java/com/betha/ouvidoria/ouvidoria/model/Endereco.java
@@ -0,0 +1,77 @@
+package com.betha.ouvidoria.ouvidoria.model;
+
+public class Endereco {
+    private Integer id;
+    private String tipologradouro;
+    private String logradouro;
+    private Integer numero;
+    private String bairro;
+    private String cidade;
+    private Integer cep;
+
+    public Endereco(Integer id, String tipologradouro, String logradouro, Integer numero, String bairro, String cidade, Integer cep) {
+        this.id = id;
+        this.tipologradouro = tipologradouro;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.cep = cep;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTipologradouro() {
+        return tipologradouro;
+    }
+
+    public void setTipologradouro(String tipologradouro) {
+        this.tipologradouro = tipologradouro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public void setCep(Integer cep) {
+        this.cep = cep;
+    }
+}
---
+++ ouvidoria/src/main/java/com/betha/ouvidoria/ouvidoria/model/Usuario.java
@@ -0,0 +1,90 @@
+package com.betha.ouvidoria.ouvidoria.model;
+
+public class Usuario {
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private String telefone;
+    private String email;
+    private String login;
+    private String senha;
+
+    public Usuario(Integer id, String nome, String cpf, String rg, String telefone, String email, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.telefone = telefone;
+        this.email = email;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
+
+
+
